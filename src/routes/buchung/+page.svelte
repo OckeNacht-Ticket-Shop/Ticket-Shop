e242a30c770a851231378a2bd848dc21ce3e0f86
I573801 <jan.wichmann@sap.com>
2025-05-14 09:05:33 +0200

    first folder structure

---
+++ src/routes/buchung/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { browser } from "$app/environment";
+	import { onMount } from "svelte";
+
+ onMount(() => {
+    if (browser) {
+        window.history.back();
+    }
+ })
+
+
+</script>
