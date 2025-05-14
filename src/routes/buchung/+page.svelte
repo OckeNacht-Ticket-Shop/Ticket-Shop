252077ca846bdae8a260b87402f382f54802165b
Jan Wichmann <133253558+wichijan@users.noreply.github.com>
2025-05-14 10:30:32 +0200

    First Folder Structure (#3)
    * first folder structure
    
    * copilot feedback
    
    * add api to folder
    
    * added test folder

---
+++ src/routes/buchung/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { browser } from "$app/environment";
+	import { onMount } from "svelte";
+
+ onMount(() => {
+    if (browser) {
+        // Navigate back in the browser history on mount to return the user to the previous page, as this page is not meant to be accessed directly.
+        window.history.back();
+    }
+ })
+
+
+</script>
